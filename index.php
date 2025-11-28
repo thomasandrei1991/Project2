e3b503ea31f1e4faa12f4c1e0e66abef496fb645
Thomas Andrei <thomasandrei1991@gmail.com>
2025-11-28 23:00:54 +0800

    All files updated

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="form-container">
+        <form action="index.php" method="post">
+            <div class="text-container">
+                <label for="firstname">Firstname : </label>
+                <input type="text" id="firstname" name="firstname">
+                <label for="lastname">Lastname : </label>
+                <input type="text" id="lastname" name="lastname">
+                <input type="submit" value="Submit">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
 <?php
-    
+    echo $_POST["firstname"];
+    echo $_POST["lastname"];
 ?>
