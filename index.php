855df0d6ac6700cf8360fdf64ec7467b07446844
Thomas Andrei <thomasandrei1991@gmail.com>
2025-11-28 22:03:04 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    $name1 = "Boygeorge";
+    $name2 = "Rhea";
+    $name3 = "Chaychay";
+    $name4 = "Ivan";
+
+    echo $name1 . "<br>";   
+    echo $name2 . "<br>";
+    echo $name3 . "<br>";
+    echo $name4 . "<br>";
+
+    if($name1 == "George") {
+        echo "Hello George!";
+    }
+    elseif($name1 == "Boygeorge") {
+        echo "Hello Boygeorge";
+    }
+    else {
+        echo    "You are not George";
+    }
+
+?>
